298a6034df245a633b01a17e4ac1fe8f3db4e3f9
Karan Kumar <102454582+AdroitNightsun@users.noreply.github.com>
2026-01-01 23:00:57 +0530

    feat: implement blog rendering functionality in JavaScript

--- responsive learning journal/script.js
+++ responsive learning journal/script.js
@@ -1,3 +1,24 @@
 const cont = document.getElementById('container')
 
-console.log(cont)
+function createBlogs(){
+    let blogHtml = ``
+    for(let i = 1; i<=6; i++){
+        blogHtml += `<div class="blog">
+          <img class="blog-img" src="images/b${i}.png" alt="blog-image" />
+          <div class="date">JULY 23, 2099</div>
+          <h1 class="blog-heading">Blog one</h1>
+          <p class="blog-p">
+            I'm excited to start a new learning journey as a Scrimba Bootcamp
+            student! After several months of learning in the Frontend Developer
+            Career Path.
+          </p>
+        </div>`
+    }
+    return blogHtml
+}
+
+function render(){
+    cont.innerHTML = createBlogs()
+}
+
+render()
